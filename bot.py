1926a0837604ea0342913ffcae8fa030e5fce33b
Mokshb6458 <115135419+Mokshb6458@users.noreply.github.com>
2022-10-20 19:27:44 +0530

    Create bot.py

---
+++ bot.py
@@ -0,0 +1,53 @@
+import asyncio
+import datetime
+import logging
+import logging.config
+import sys
+
+from pyrogram import Client
+from pyrogram.errors.exceptions.not_acceptable_406 import ChannelPrivate
+
+from config import *
+from database import db
+from database.users import filter_users
+from helpers import ping_server, temp
+from pyshortner import broadcast_admins
+
+# Get logging configurations
+logging.config.fileConfig('logging.conf')
+logging.getLogger().setLevel(logging.INFO)
+
+
+
+
+
+class Bot(Client):
+
+    def __init__(self):
+        super().__init__(
+        "shortener",
+        api_id=API_ID,
+        api_hash=API_HASH,
+        bot_token=BOT_TOKEN,
+        plugins=dict(root="plugins")
+        )
+
+    async def start(self):
+        me = await self.get_me()
+        self.owner = await self.get_users(int(OWNER_ID))
+        self.username = f'@{me.username}'
+        temp.BOT_USERNAME = me.username
+        temp.FIRST_NAME = me.first_name
+        if not await db.get_bot_stats():
+            await db.create_stats()
+        banned_users = await filter_users({"banned": True})
+        async for user in banned_users:
+            temp.BANNED_USERS.append(user["user_id"])
+        logging.info(LOG_STR)
+        await broadcast_admins(self, '** Bot started successfully **\n\nBot By @GreyMatter_Bots')
+        logging.info('Bot started\n\nBot By @GreyMatter_Bots')
+
+    async def stop(self, *args):
+        await broadcast_admins(self, '** Bot Stopped Bye **\n\nBot By @GreyMatter_Bots')
+        await super().stop()
+        logging.info('Bot Stopped Bye\n\nBot By @GreyMatter_Bots')
